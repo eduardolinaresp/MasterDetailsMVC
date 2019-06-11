a7b329aa8fe8f90becc22d196b6ab87f347870e3
Eduardo <eduardo.linares@outlook.com>
2019-06-11 10:38:30 -0400

    ADD CopyRoslynFiles to project file /output folder

--- MasterDetails/Scripts/ProyectScripts/Purchase/PurchaseCreate.js
+++ MasterDetails/Scripts/ProyectScripts/Purchase/PurchaseCreate.js
@@ -1,6 +1,6 @@
 ﻿
 
-$("#AddButton").click(function() {
+$("#AddButton").click(function () {
 
     createdRowPurchase();
 
@@ -21,24 +21,24 @@ function createdRowPurchase() {
 
     var ItemNameCell = "<td><input type='hidden' id='ItemName" + index + "' name='PurchaseDetailses[" + index + "].Name' value='" + selectedItem.ItemName + "'/>" + selectedItem.ItemName + " </td>";
 
-    
-    console.log(selectedItem.ItemName); 
+
+    console.log(selectedItem.ItemName);
 
     var ItemQtyCell = "<td><input type='hidden' id='ItemQty" + index + "' name='PurchaseDetailses[" + index + "].Qty' value='" + selectedItem.ItemQty + "'/>" + selectedItem.ItemQty + " </td>";
 
     var ItemPriceCell = "<td><input type='hidden' id='ItemPrice" + index + "' name='PurchaseDetailses[" + index + "].Price' value='" + selectedItem.ItemPrice + "'/>" + selectedItem.ItemPrice + " </td>";
 
     var createdNewRow = "<tr>" + indexCell + SerialCell + ItemNameCell + ItemQtyCell + ItemPriceCell + "</tr>";
-   
-    console.log(ItemNameCell); 
+
+    console.log(ItemNameCell);
     console.log(ItemQtyCell);
     console.log(ItemPriceCell);
 
     $("#PurchaseDetailsTable").append(createdNewRow);
 
-    //$("#ItemName").val("");
-    //$("#ItemQty").val("");
-    //$("#ItemPrice").val("");
+    $("#ItemName").val("") ="";
+    $("#ItemQty").val("");
+    $("#ItemPrice").val("");
 
 
 }
